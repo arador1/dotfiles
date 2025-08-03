0454ec2406fc8cd60f3acf68b56fb95c319b65f3
arador1 <chaitanyachandu523@gmail.com>
2025-08-03 22:39:45 +0530

    ~

--- rofi/power.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-#
-chosen=$(printf "󰐥 Power Off\n Restart" | rofi -dmenu -i -l 3)
-case "$chosen" in
-"󰐥 Power Off") loginctl poweroff ;;
-" Restart") loginctl reboot ;;
-*) exit 1 ;;
-esac
